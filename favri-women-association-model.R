8b7fcbd09856b33f288c42a4af74aaaf069f7ee7
CWWhitney <cory.whitney@gmail.com>
2023-07-26 14:10:14 +0700

    Added an input file and more clarification on decision option

--- favri-women-association-model.R
+++ favri-women-association-model.R
@@ -1,1 +1,11 @@
-# favri women assocition
+# FAVRI Women's association
+# mobile selling of safe vegetables
+
+
+make_variables <- function(est,n=1)
+{x <- random(rho=est,n=n)
+for(i in colnames(x))assign(i, as.numeric(x[1,i]),envir=.GlobalEnv)}
+
+make_variables(estimate_read_csv(paste("",sep="")))
+
+
