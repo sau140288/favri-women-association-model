e7bfc1794d3477e35673baa91130fea28dac683a
CWWhitney <cory.whitney@gmail.com>
2023-07-26 14:13:49 +0700

    add input table refence in the script

--- favri-women-association-model.R
+++ favri-women-association-model.R
@@ -1,11 +1,11 @@
 # FAVRI Women's association
 # mobile selling of safe vegetables
 
-
+library(decisionSupport)
 make_variables <- function(est,n=1)
 {x <- random(rho=est,n=n)
 for(i in colnames(x))assign(i, as.numeric(x[1,i]),envir=.GlobalEnv)}
 
-make_variables(estimate_read_csv(paste("",sep="")))
+make_variables(estimate_read_csv(paste("inputs_women_assoc.csv",sep="")))
 
 
